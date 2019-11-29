6ff0e3c404161c9a51fd642cbbdb38f200ad6abc
lichaochao <lichaochao62@163.com>
2019-11-30 00:15:21 +0800

    add echarts

--- src/components/DataView.vue
+++ src/components/DataView.vue
@@ -1,56 +1,175 @@
 <template>
-    <div>
-        <h1>{{content.title}}</h1>
-        <el-tabs type="border-card " v-if="content.menu.length >=1" @tab-click="handleClick">
-      <el-tab-pane :label="item" v-for="(item,index) in content.menu" :key="index"></el-tab-pane>
+  <div>
+    <h1>{{content.title}}</h1>
+    <el-tabs
+      type="border-card "
+      v-model="menuactive"
+      v-if="content.menu.length >=1"
+      @tab-click="handleClick"
+    >
+      <el-tab-pane :label="item" :name="item" v-for="(item,index) in content.menu" :key="index"></el-tab-pane>
     </el-tabs>
-        <div v-show="showtable"><h1>{{actdata.echart}}</h1></div>
-        <div v-show="!showtable"><h1>{{actdata.table}}</h1></div>
-        <el-button type="primary" @click="changed()">切换</el-button>
+    <div v-show="showtable">
+      <div :id="echartid" ref="echartdiv" style="width:500px;height:400px"></div>
     </div>
+    <div v-show="showtable">
+      <template v-if=" actdata &&  actdata.tabledata  && actdata.tabledata.data.length>0 ">
+        <el-table :data="actdata.tabledata.data">
+          <template v-for="(item,index) in actdata.tabledata.lables">
+            <el-table-column :key="index" :prop="item.prop" :label="item.label"></el-table-column>
+          </template>
+        </el-table>
+      </template>
+    </div>
+    <el-button type="primary" @click="changed()">切换</el-button>
+  </div>
 </template>
 <script>
-    export default {
-      data() {
-        return {  
-            actdata:{
-                echart:'',table:''
-            }        
-        }
+import echart from "echarts";
+export default {
+  data() {
+    return {
+      actdata: {
+        echart: [],
+        tabledata: { lables: [], data: [] }
       },
-      props:{
-          showtable:{
-              type:Boolean,
-              default:false,
-          },
-          title:{
-              type:String,
-              default:''
-          },
-          content:{
-              type:Object,
-              default:()=>{
-                  return {
-                      title:'',
-                      menu:[],
-                      data:{echart:'',table:''}
-                  }
-              }
-          }
+      menuactive: "",
+      actNames: [],
+
+      myChart: {},
+      echartid: ""
+    };
+  },
+  watch: {
+    content: {
+      handler(val) {
+        console.log(val);
+        this.menuactive = val.menu[0];
+        this.actdata = val.data[0];
+        this.chengedecharts(this.actdata);
       },
-      mounted(){
-        this.actdata = this.content.data[0];
-        console.log(this.content)
-      },
-      methods:{
-          changed()
-          {
-              this.showtable=!this.showtable
-          },
-          handleClick(tab)
-          {
-              this.actdata = this.content.data[tab.index];
-          }
+      deep: true
+    }
+  },
+  computed: {},
+  props: {
+    showtable: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: ""
+    },
+    content: {
+      type: Object,
+      default: () => {
+        return {
+          title: "",
+          menu: [],
+          data: { echart: "", tabledata: { lables: [], data: [] } }
+        };
       }
     }
-  </script>
+  },
+  mounted() {
+    this.actdata = this.content.data[0];
+    this.echartid = "t" + Math.random();
+
+    this.chengedecharts(this.actdata);
+  },
+  methods: {
+    changed() {
+      this.showtable = !this.showtable;
+    },
+    handleClick(tab) {
+      this.actdata = this.content.data[tab.index];
+      this.echartid = "t" + Math.random();
+      this.chengedecharts(this.actdata);
+    },
+    chengedecharts(actdata) {
+      this.actNames = [];
+      
+      actdata.echart.forEach(element => {
+        if (element.type == "bar") {
+          element.barWidth = "10";
+        }
+        this.actNames.push(element.name);
+      });
+      let option = {
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            crossStyle: {
+              color: "#999"
+            }
+          }
+        },
+        toolbox: {
+          feature: {
+            dataView: { show: true, readOnly: false },
+            magicType: { show: true, type: ["line", "bar"] },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        legend: {
+          data: []
+        },
+        xAxis: [
+          {
+            type: "category",
+            data: [
+              "1月",
+              "1月",
+              "1月",
+              "1月",
+              "1月",
+              "1月",
+              "1月",
+              "1月",
+              "1月",
+              "1月",
+              "1月",
+              "1月"
+            ],
+            axisPointer: {
+              type: "shadow"
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: "value",
+            name: "水量",
+            min: 0,
+            max: 250,
+            interval: 50,
+            axisLabel: {
+              formatter: "{value} ml"
+            }
+          }
+        ],
+        series: []
+      };
+
+      option.legend.data = this.actNames;
+      if (actdata.echart.length > 0) {
+        option.series = actdata.echart;
+      } else {
+        option.series = [];
+      }
+
+      if (this.myChart) {
+        this.myChart = echart.init(this.$refs.echartdiv);
+      }
+      this.myChart.setOption(option);
+
+      // 绘制图表
+      console.log(actdata);
+      console.log(option);
+    }
+  }
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <el-tabs type="border-card " @tab-click="handleMainClick">
+    <!-- <el-tabs type="border-card " @tab-click="handleMainClick">
       <el-tab-pane :label="sub.title" v-for="(sub,subindex) in pagedata" :key="subindex"></el-tab-pane>
-    </el-tabs>
+    </el-tabs> -->
+    <button @click="kongtiaochenged">空调</button>
+    <button @click="xiyijichenged">洗衣机</button>
+    <button @click="bingxiangchenged">冰箱</button>
     <el-tabs type="border-card " v-if="actsubdata.isshow" @tab-click="handleSubClick">
       <el-tab-pane :label="item.title" v-for="(item,index) in actsubdata.contents" :key="index"></el-tab-pane>
     </el-tabs>
-    <DataView :showtable="true" v-for="(item,index) in details"  :key="index" :content="item"></DataView>
+
+    <DataView :showtable="true" v-for="(item,index) in details" :key="index" :content.sync="item"></DataView>
   </div>
 </template>
 
@@ -21,17 +25,177 @@ export default {
   props: {},
   data() {
     return {
-      pagedata: [{
+      pagedata: [
+        {
           title: "空调",
           children: {
             isshow: true,
             contents: [
-              { title: "App控制",children: { isshow: true, contents: [{title:'效率1',menu:['功率1','功率2','功率3'],data:[{echart:'功率1echart',table:'功率1-table'},{echart:'功率2echart',table:'功率2-table'},{echart:'功率3echart',table:'功率3-table'}]},
-                                                                        {title:'效率2',menu:[],data:[{echart:'echart',table:'table'}]},
-                                                                        {title:'效率3',menu:[],data:[{echart:'echart',table:'table'}]}
-                                                                        ] } },
-              { title: "大屏控制", children: { isshow: true, contents: [{title:'效率2',menu:[],data:[{echart:'echart',table:'table'}]}] } },
-              { title: "音响控制", children: { isshow: true, contents: [{title:'效率2',menu:[],data:[{echart:'echart',table:'table'}]}] } }
+              {
+                title: "App控制",
+                children: {
+                  isshow: true,
+                  contents: [
+                    {
+                      title: "效率1",
+                      menu: ["功率1", "功率2", "功率3"],
+                      data: [
+                        { echart: [ {
+                                  name:'蒸发量',
+                                  type:'bar',
+                                  data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
+                                  barWidth:'30'
+                              },
+                              {
+                                  name:'降水量',
+                                  type:'bar',
+                                  data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+                                  barWidth:'30'
+                              },
+                              {
+                                  name:'平均温度',
+                                  type:'line',
+                                  data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+                              }], 
+                            tabledata: {
+                                  lables: [
+                                    { label: "用户ID", prop: "uid" },
+                                    { label: "名称", prop: "cname" },
+                                    { label: "性别", prop: "sex" }
+                            ],
+                            data: [
+                              { uid: 1, cname: "生命一号", sex: "瓶装" },
+                              { uid: 2, cname: "生命二号", sex: "瓶装" },
+                              { uid: 3, cname: "生命三号", sex: "片装" }
+                            ]
+                          } },
+                        { echart: [ {
+                                  name:'排水量',
+                                  type:'bar',
+                                  data:[112.0, 41.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
+                                  barWidth:'30'
+                              },
+                              {
+                                  name:'污水量',
+                                  type:'bar',
+                                  data:[112.6, 51.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+                                  barWidth:'30'
+                              },
+                              {
+                                  name:'平均量',
+                                  type:'line',
+                                  data:[12.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+                              }], tabledata: {
+                            lables: [
+                              { label: "班级", prop: "id" },
+                              { label: "学生", prop: "stuents" },
+                              { label: "老师", prop: "teacher" }
+                            ],
+                            data: [
+                              { id: 1, stuents: 50, teacher: "李老师" },
+                              { id: 5, stuents: 55, teacher: "杨老师" },
+                              { id: 3, stuents: 60, teacher: "孙老师" }
+                            ]
+                          } },
+                        { echart: [], tabledata: {
+                            lables: [
+                              { label: "年级", prop: "id" },
+                              { label: "班数", prop: "classes" },
+                              { label: "教导主任", prop: "teacher" }
+                            ],
+                            data: [
+                              { id: 1, classes: 10, teacher: "王主任" },
+                              { id: 2, classes: 12, teacher: "陈主任" },
+                              { id: 3, classes: 1, teacher: "副校长" }
+                            ]
+                          }  }
+                      ]
+                    },
+                    {
+                      title: "效率2",
+                      menu: [],
+                      data: [{ echart: [], tabledata: {
+                            lables: [
+                              { label: "商品ID3", prop: "id" },
+                              { label: "商品名称3", prop: "name" },
+                              { label: "类型3", prop: "specs" }
+                            ],
+                            data: [
+                              { id: 1, name: "生命一号3", specs: "瓶装" },
+                              { id: 2, name: "生命二号3", specs: "瓶装" },
+                              { id: 3, name: "生命三号3", specs: "片装" }
+                            ]
+                          } }]
+                    },
+                    {
+                      title: "效率3",
+                      menu: [],
+                      data: [{ echart: [], tabledata: {
+                            lables: [
+                              { label: "商品ID4", prop: "id" },
+                              { label: "商品名称4", prop: "name" },
+                              { label: "类型4", prop: "specs" }
+                            ],
+                            data: [
+                              { id: 1, name: "生命一号4", specs: "瓶装" },
+                              { id: 2, name: "生命二号4", specs: "瓶装" },
+                              { id: 3, name: "生命三号4", specs: "片装" }
+                            ]
+                          } }]
+                    }
+                  ]
+                }
+              },
+              {
+                title: "大屏控制",
+                children: {
+                  isshow: true,
+                  contents: [
+                    {
+                      title: "效率2",
+                      menu: [],
+                      data: [{echart: [],tabledata: {
+                            lables: [
+                              { label: "商品ID", prop: "id" },
+                              { label: "商品名称", prop: "name" },
+                              { label: "类型", prop: "specs" }
+                            ],
+                            data: [
+                              { id: 1, name: "生命一号5", specs: "瓶装" },
+                              { id: 2, name: "生命二号5", specs: "瓶装" },
+                              { id: 3, name: "生命三号5", specs: "片装" }
+                            ]
+                          }
+                        }
+                      ]
+                    }
+                  ]
+                }
+              },
+              {
+                title: "音响控制",
+                children: {
+                  isshow: true,
+                  contents: [
+                    {
+                      title: "效率2",
+                      menu: [],
+                      data: [{ echart: [], tabledata: {
+                            lables: [
+                              { label: "商品1ID", prop: "id" },
+                              { label: "商品1名称", prop: "name" },
+                              { label: "类型1", prop: "specs" }
+                            ],
+                            data: [
+                              { id: 1, name: "生命1一号6", specs: "瓶装" },
+                              { id: 2, name: "生命1二号6", specs: "瓶装" },
+                              { id: 3, name: "生命1三号6", specs: "片装" }
+                            ]
+                          } }]
+                    }
+                  ]
+                }
+              }
             ]
           }
         },
@@ -42,19 +206,93 @@ export default {
             contents: [
               {
                 title: "品牌",
-                children: {isshow: true,
-                  contents: [{title:'效率2',menu:[],data:[{echart:'echart',table:'table'}]},
-                  {title:'效率2',menu:[],data:[{echart:'echart',table:'table'}]},
-                  {title:'效率2',menu:[],data:[{echart:'echart',table:'table'}]}
-                ]}
+                children: {
+                  isshow: true,
+                  contents: [
+                    {
+                      title: "效率2",
+                      menu: [],
+                      data: [{},
+                          { echart: [], tabledata: {
+                            lables: [
+                              { label: "海信洗衣机ID", prop: "id" },
+                              { label: "海信商品名称", prop: "name" },
+                              { label: "海信类型", prop: "specs" }
+                            ],
+                            data: [
+                              { id: 1, name: "海信洗衣机1", specs: "瓶装" },
+                              { id: 2, name: "海信洗衣机2", specs: "瓶装" },
+                              { id: 3, name: "海信洗衣机3", specs: "片装" }
+                            ]
+                          } }
+                          ]
+                    },
+                    {
+                      title: "效率2",
+                      menu: ['海尔1','海信1'],
+                      data: [{ echart: [], tabledata: {
+                            lables: [
+                              { label: "海尔1商品ID", prop: "id" },
+                              { label: "海尔1洗衣机名称", prop: "name" },
+                              { label: "海尔1类型", prop: "specs" }
+                            ],
+                            data: [
+                              { id: 1, name: "海尔1洗衣机4", specs: "瓶装" },
+                              { id: 2, name: "海尔1洗衣机7", specs: "瓶装" },
+                              { id: 3, name: "海尔1洗衣机6", specs: "片装" }
+                            ]
+                          } },
+                          { echart: [], tabledata: {
+                            lables: [
+                              { label: "海信1商品ID", prop: "id" },
+                              { label: "海信1洗衣机名称", prop: "name" },
+                              { label: "海信1类型", prop: "specs" }
+                            ],
+                            data: [
+                              { id: 1, name: "海信1洗衣机4", specs: "瓶装" },
+                              { id: 2, name: "海信1洗衣机7", specs: "瓶装" },
+                              { id: 3, name: "海信1洗衣机6", specs: "片装" }
+                            ]
+                          } }]
+                    },
+                    {
+                      title: "效率2",
+                      menu: [],
+                      data: [{ echart:[], tabledata: {
+                            lables: [
+                              { label: "商品ID", prop: "id" },
+                              { label: "商品名称", prop: "name" },
+                              { label: "洗衣机", prop: "specs" }
+                            ],
+                            data: [
+                              { id: 1, name: "洗衣机8", specs: "瓶装" },
+                              { id: 2, name: "洗衣机9", specs: "瓶装" },
+                              { id: 3, name: "洗衣机10", specs: "片装" }
+                            ]
+                          } }]
+                    }
+                  ]
+                }
               },
               {
                 title: "产地",
                 children: {
                   isshow: true,
-                  contents: [{
-                      title: "吉林省",children: { isshow: true, contents: [{title:'效率2',menu:[],data:[{echart:'echart',table:'table'}]}] }
-                    }]
+                  contents: [
+                    {
+                      title: "吉林省",
+                      children: {
+                        isshow: true,
+                        contents: [
+                          {
+                            title: "效率2",
+                            menu: [],
+                            data: [{ echart: "echart", table: "table" }]
+                          }
+                        ]
+                      }
+                    }
+                  ]
                 }
               }
             ]
@@ -88,24 +326,28 @@ export default {
   mounted() {
     this.actsubdata = this.pagedata[0].children;
     this.details = this.actsubdata.contents[0].children.contents;
-    console.log(this.details);
     console.log("hello");
   },
   methods: {
     //第一层tab切换
-    handleMainClick(tab, event) {
-      this.actsubdata = this.pagedata[tab.index].children;
-      console.log(this.actsubdata);
-      this.details = this.actsubdata.contents[0].children.contents;
-      console.log(tab, event);
-    },
+    // handleMainClick(tab, event) {
+    //   this.actsubdata = this.pagedata[tab.index].children;
+    //   this.details = this.actsubdata.contents[0].children.contents;
+    // },
     //第二层tab切换
     handleSubClick(tab) {
-      console.log(tab.index)
-      console.log(this.actsubdata);
-      this.details = this.actsubdata.contents[tab.index].children.contents; 
-       console.log(this.details);
-    }
+      this.details = JSON.parse(JSON.stringify(this.actsubdata.contents[tab.index].children.contents));
+    },
+    kongtiaochenged()
+    {
+      
+    },
+    xiyijichenged(){
+
+    },
+    bingxiangchenged(){
+
+    } 
   }
 };
 </script>
